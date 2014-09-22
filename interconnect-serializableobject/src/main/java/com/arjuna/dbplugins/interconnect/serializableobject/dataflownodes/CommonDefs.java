236e8f7bd06de9e32359b7653852ff67209204d4
Stuart Wheater <stuart.wheater@arjuna.com>
2014-09-22 16:01:53 +0100

    Changes, but more work needed

--- interconnect-serializableobject/src/main/java/com/arjuna/dbplugins/interconnect/serializableobject/dataflownodes/CommonDefs.java
+++ interconnect-serializableobject/src/main/java/com/arjuna/dbplugins/interconnect/serializableobject/dataflownodes/CommonDefs.java
@@ -6,17 +6,21 @@ package com.arjuna.dbplugins.interconnect.serializableobject.dataflownodes;
 
 public class CommonDefs
 {
-    public static final String INTERCONNECT_NAMESPACE                          = "http://databroker.arjuna.com/interconnect/";
+    public static final String INTERCONNECT_NAMESPACE                                     = "http://databroker.arjuna.com/interconnect/";
 
-    public static final String INTERCONNECT_SERVICE_PATH                       = "serializableobject_interconnect";
+    public static final String INTERCONNECT_SERVICE_PATH                                  = "serializableobject_interconnect";
 
-    public static final String INTERCONNECT_SERVICENAME_ACCEPTOR               = "AcceptorService";
-    public static final String INTERCONNECT_PORTNAME_ACCEPTOR                  = "AcceptorPort";
-    public static final String INTERCONNECT_OPERATIONNAME_ACCEPTOR_RECEIVEDATA = "ReceiveData";
+    public static final String INTERCONNECT_SERVICENAME_ACCEPTOR                          = "AcceptorService";
+    public static final String INTERCONNECT_PORTNAME_ACCEPTOR                             = "AcceptorPort";
+    public static final String INTERCONNECT_OPERATIONNAME_ACCEPTOR_RECEIVEDATA            = "ReceiveData";
 
-    public static final String INTERCONNECT_SERVICENAME_PROVIDER               = "ProviderService";
-    public static final String INTERCONNECT_PORTNAME_PROVIDER                  = "ProviderPort";
-    public static final String INTERCONNECT_OPERATIONNAME_PROVIDER_OBTAINDATA  = "ObtainData";
-    
-    public static final String INTERCONNECT_PARAMETERNAME_ID                   = "id";
+    public static final String INTERCONNECT_RECEIVEDATA_PARAMETERNAME_ID                  = "id";
+    public static final String INTERCONNECT_RECEIVEDATA_PARAMETERNAME_SERIALIALIZEDOBJECT = "serialializedobject";
+
+    public static final String INTERCONNECT_SERVICENAME_PROVIDER                          = "ProviderService";
+    public static final String INTERCONNECT_PORTNAME_PROVIDER                             = "ProviderPort";
+    public static final String INTERCONNECT_OPERATIONNAME_PROVIDER_OBTAINDATA             = "ObtainData";
+
+    public static final String INTERCONNECT_OBTAINDATA_PARAMETERNAME_ID                   = "id";
+    public static final String INTERCONNECT_OBTAINDATA_PARAMETERNAME_SERIALIALIZEDOBJECT  = "serialializedobject";
 }
--- interconnect-serializableobject/src/main/java/com/arjuna/dbplugins/interconnect/serializableobject/dataflownodes/SerializableObjectPullDataSource.java
+++ interconnect-serializableobject/src/main/java/com/arjuna/dbplugins/interconnect/serializableobject/dataflownodes/SerializableObjectPullDataSource.java
@@ -9,14 +9,17 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
 import javax.xml.soap.SOAPConnection;
 import javax.xml.soap.SOAPConnectionFactory;
 import javax.xml.soap.SOAPConstants;
@@ -24,6 +27,7 @@ import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.soap.SOAPPart;
+
 import com.arjuna.databroker.data.DataFlow;
 import com.arjuna.databroker.data.DataProvider;
 import com.arjuna.databroker.data.DataSource;
@@ -62,13 +66,13 @@ public class SerializableObjectPullDataSource extends TimerTask implements DataS
     @Override
     public DataFlow getDataFlow()
     {
-    	return _dataFlow;
+        return _dataFlow;
     }
 
     @Override
     public void setDataFlow(DataFlow dataFlow)
     {
-    	_dataFlow = dataFlow;
+        _dataFlow = dataFlow;
     }
 
     @Override
@@ -109,7 +113,7 @@ public class SerializableObjectPullDataSource extends TimerTask implements DataS
             SOAPEnvelope   requestEnvelope  = requestPart.getEnvelope();
             SOAPBody       requestBody      = requestEnvelope.getBody();
             SOAPElement    requestElement   = requestBody.addChildElement(CommonDefs.INTERCONNECT_OPERATIONNAME_PROVIDER_OBTAINDATA, "ic", CommonDefs.INTERCONNECT_NAMESPACE);
-            SOAPElement    requestParameter = requestElement.addChildElement(CommonDefs.INTERCONNECT_PARAMETERNAME_ID, "ic");
+            SOAPElement    requestParameter = requestElement.addChildElement(CommonDefs.INTERCONNECT_OBTAINDATA_PARAMETERNAME_ID, "ic");
             requestParameter.addTextNode(_endpointPath);
 
             if (logger.isLoggable(Level.FINER))
@@ -133,12 +137,27 @@ public class SerializableObjectPullDataSource extends TimerTask implements DataS
                 responceOutputStream.close();
             }
 
-            SOAPPart     responcePart     = responce.getSOAPPart();
-            SOAPEnvelope responceEnvelope = responcePart.getEnvelope();
-            SOAPBody     responceBody     = responceEnvelope.getBody();
+            SOAPPart     responcePart         = responce.getSOAPPart();
+            SOAPEnvelope responceEnvelope     = responcePart.getEnvelope();
+            SOAPBody     responceBody         = responceEnvelope.getBody();
+            Iterator<?>  responceBodyChildren = responceBody.getChildElements();
 
-            if (responceBody.hasChildNodes())
-                result = responceBody.extractContentAsDocument();
+            if (responceBodyChildren.hasNext())
+            {
+                SOAPBodyElement dataElement = (SOAPBodyElement) responceBodyChildren.next();
+
+                if ((dataElement.getNodeType() == SOAPBodyElement.ELEMENT_NODE) &&  dataElement.getNodeName().equals(CommonDefs.INTERCONNECT_RECEIVEDATA_PARAMETERNAME_ID))
+                {
+                    result = dataElement.getValue();
+                }
+                else
+                    logger.log(Level.WARNING, "More than one child in responce body");
+
+                if (responceBodyChildren.hasNext())
+                    logger.log(Level.WARNING, "More than one child in responce body");
+           }
+            else
+                logger.log(Level.WARNING, "No child in responce body");
         }
         catch (Throwable throwable)
         {
